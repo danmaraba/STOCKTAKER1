20eba6784f9a7bfc4530e74971e7e2dc4bf04b83
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-09-07 15:45:46 +0300

    created a table of employees

--- stocktaker-server/db/migrate/20220907120848_create_employees.rb
+++ stocktaker-server/db/migrate/20220907120848_create_employees.rb
@@ -1,4 +1,11 @@
 class CreateEmployees < ActiveRecord::Migration[6.1]
   def change
+    create_table :employees do |t|
+      t.string :name
+      t.string :gender
+      t.integer :employee_id
+      t.string :department
+      t.timestamps
+    end
   end
 end
