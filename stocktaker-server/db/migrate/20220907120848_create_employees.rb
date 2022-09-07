a55d68a8a104a25d82d4d6095b9322e71f8b8c4e
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-09-07 16:33:52 +0300

    updated the employees migration file

--- stocktaker-server/db/migrate/20220907120848_create_employees.rb
+++ stocktaker-server/db/migrate/20220907120848_create_employees.rb
@@ -3,7 +3,6 @@ class CreateEmployees < ActiveRecord::Migration[6.1]
     create_table :employees do |t|
       t.string :name
       t.string :gender
-      t.integer :employee_id
       t.string :department
       t.timestamps
     end
--- stocktaker-server/db/migrate/20220907123051_create_roles.rb
+++ stocktaker-server/db/migrate/20220907123051_create_roles.rb
@@ -1,4 +1,8 @@
 class CreateRoles < ActiveRecord::Migration[6.1]
   def change
+    create_table :roles do |t|
+      t.string :task
+      t.integer :employee_id
+      t.integer :product_id
   end
 end
